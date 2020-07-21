e4c8a7e3d6c706143d21bd49c0017c8c281b15d6
JulianNavi <39164188+JulianNavi@users.noreply.github.com>
2020-07-21 21:43:20 +0300

    Update App.tsx

--- reactquiz-ts/src/App.tsx
+++ reactquiz-ts/src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTION = 20;
+
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -16,7 +25,14 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Question ...</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTION}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next question
       </button>
